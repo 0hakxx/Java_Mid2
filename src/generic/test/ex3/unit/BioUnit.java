8369cebf426ad98cfb41d609a55e5431d57f649a
yeong <yeonghak2207@gmail.com>
2025-03-06 19:11:18 +0900

    Second Commit Message

---
+++ src/generic/test/ex3/unit/BioUnit.java
@@ -0,0 +1,26 @@
+package generic.test.ex3.unit;
+
+public class BioUnit {
+    private String name;
+    private int hp;
+
+    public BioUnit(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public int getHp() {
+        return this.hp;
+    }
+
+    @Override
+    public String toString() {
+        return "BioUnit{" +
+                "name='" + name + '\'' +
+                ", hp=" + hp +
+                '}';
+    }
+}
---
+++ src/generic/test/ex3/unit/Marine.java
@@ -0,0 +1,11 @@
+package generic.test.ex3.unit;
+
+import generic.animal.Animal;
+
+public class Marine extends BioUnit {
+    public Marine(String name, int hp) {
+        super(name, hp);
+    }
+
+
+}
