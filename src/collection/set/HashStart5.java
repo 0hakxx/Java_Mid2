8c3468bca19b53206112203026dd5cd9e68fce60
yeong <yeonghak2207@gmail.com>
2025-03-12 15:39:07 +0900

    4th Commit Message

--- src/collection/set/HashStart5.java
+++ src/collection/set/HashStart5.java
@@ -1,17 +1,19 @@
 package collection.set;
 
-
+import java.util.Arrays;
 import java.util.LinkedList;
 
-
 public class HashStart5 {
-    static final int CAPCITY = 10;
 
-    public static void main(String[] args){
-        LinkedList<Integer>[] buckets = new LinkedList[10];
-        for(int i=0; i<buckets.length; i++){
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+        //{1, 2, 5, 8, 14, 99}
+        LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+        for (int i = 0; i < CAPACITY; i++) {
             buckets[i] = new LinkedList<>();
         }
+
         add(buckets, 1);
         add(buckets, 2);
         add(buckets, 5);
@@ -19,13 +21,29 @@ public class HashStart5 {
         add(buckets, 14);
         add(buckets, 99);
         add(buckets, 9); //중복
+        System.out.println("buckets = " + Arrays.toString(buckets));
+
+        //검색
+        int searchValue = 9;
+        boolean contains = contains(buckets, searchValue);
+        System.out.println("bucket.contains(" + searchValue + ") = " + contains);
     }
 
-    public static void add(LinkedList<Integer>[] buckets, int searchVaalue){
-        int hashIndex = hashIndex(int searchValue);
-    }
-    static int hashIndex(int hashValue){
-        return hashValue % CAPCITY;
+    private static void add(LinkedList<Integer>[] buckets, int value) {
+        int hashIndex = hashIndex(value);
+        LinkedList<Integer> bucket = buckets[hashIndex]; // O(1)
+        if (!bucket.contains(value)) { // O(n)
+            bucket.add(value);
+        }
     }
 
+    private static boolean contains(LinkedList<Integer>[] buckets, int searchValue) {
+        int hashIndex = hashIndex(searchValue);
+        LinkedList<Integer> bucket = buckets[hashIndex]; // O(1)
+        return bucket.contains(searchValue); // O(n)
+    }
+
+    static int hashIndex(int value) {
+        return value % CAPACITY;
+    }
 }
---
+++ src/collection/set/MyHashSetV2Main2.java
@@ -0,0 +1,26 @@
+package collection.set;
+
+import collection.set.member.Member;
+
+public class MyHashSetV2Main2 {
+
+    public static void main(String[] args) {
+        MyHashSetV2 set = new MyHashSetV2(10);
+        Member hi = new Member("hi");
+        Member jpa = new Member("jpa");
+        Member spring = new Member("spring");
+        Member java = new Member("java");
+        System.out.println("hi.hashCode() = " + hi.hashCode());
+        System.out.println("jpa.hashCode() = " + jpa.hashCode());
+        System.out.println("java.hashCode() = " + java.hashCode());
+        System.out.println("spring.hashCode() = " + spring.hashCode());
+
+        set.add(hi);
+        set.add(jpa);
+        set.add(spring);
+        set.add(java);
+        System.out.println(set);
+
+
+    }
+}
