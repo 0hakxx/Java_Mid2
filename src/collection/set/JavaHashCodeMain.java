0691d0c2671fee3d15f432b33648b0b505d630d0
yeong <yeonghak2207@gmail.com>
2025-04-23 15:15:27 +0900

    Perfected Last Version Commit Message

--- src/collection/set/JavaHashCodeMain.java
+++ src/collection/set/JavaHashCodeMain.java
@@ -6,30 +6,39 @@ public class JavaHashCodeMain {
 
     public static void main(String[] args) {
         //Object의 기본 hashCode는 객체의 참조값을 기반으로 생성
-        Object obj1 = new Object();
-        Object obj2 = new Object();
+        Object obj1 = new Object(); // 새로운 Object 인스턴스 생성
+        Object obj2 = new Object(); // 또 다른 Object 인스턴스 생성
+        // 각각의 객체는 서로 다른 메모리 주소(참조값)를 가지므로, hashCode 값도 다르다.
         System.out.println("obj1.hashCode() = " + obj1.hashCode());
         System.out.println("obj2.hashCode() = " + obj2.hashCode());
 
         //각 클래스마다 hashCode를 이미 오버라이딩 해두었다.
-        Integer i = 10;
-        String strA = "A";
+        Integer i = 10; // Integer 클래스는 hashCode를 값 자체로 반환하도록 오버라이딩되어 있다.
+        String strA = "A"; // String 클래스도 hashCode를 오버라이딩해서 문자열의 내용을 기반으로 해시값을 계산한다.
         String strAB = "AB";
-        System.out.println("10.hashCode = " + i.hashCode());
-        System.out.println("'A'.hashCode = " + strA.hashCode());
-        System.out.println("'AB'.hashCode = " + strAB.hashCode());
+        // Integer, String 등 주요 표준 클래스들은 hashCode와 equals를 적절히 오버라이딩하여, 값이 같으면 해시값도 같게 동작한다.
+        System.out.println("10.hashCode = " + i.hashCode()); // 10의 해시코드(=10)
+        System.out.println("'A'.hashCode = " + strA.hashCode()); // 'A'의 해시코드
+        System.out.println("'AB'.hashCode = " + strAB.hashCode()); // 'AB'의 해시코드
 
         //hashCode는 마이너스 값이 들어올 수 있다.
-        System.out.println("-1.hashCode = " + Integer.valueOf(-1).hashCode());
+        // 해시코드는 int 범위 내에서 어떤 값도 나올 수 있으므로, 음수값도 가능하다.
+        System.out.println("-1.hashCode = " + Integer.valueOf(-1).hashCode()); // -1의 해시코드(=-1)
 
         //둘은 같을까 다를까?, 인스턴스는 다르지만, equals는 같다.
-        Member member1 = new Member("idA");
-        Member member2 = new Member("idA");
+        Member member1 = new Member("idA"); // idA라는 id를 가진 Member 인스턴스 생성
+        Member member2 = new Member("idA"); // 동일한 id를 가진 또 다른 Member 인스턴스 생성
 
         //equals, hashCode를 오버라이딩 하지 않은 경우와, 한 경우를 비교
+        // == 연산자는 두 객체의 참조(주소)가 같은지 비교한다. (항상 false)
         System.out.println("(member1 == member2) = " + (member1 == member2));
+        // equals 메서드는 두 객체의 논리적 동등성을 비교한다.
+        // Member 클래스에서 equals를 오버라이딩했다면 id 값이 같으면 true, 아니면 false가 된다.
         System.out.println("member1 equals member2 = " + member1.equals(member2));
+        // hashCode도 오버라이딩했다면, id가 같을 때 같은 해시코드가 반환된다.
         System.out.println("member1.hashCode() = " + member1.hashCode());
         System.out.println("member2.hashCode() = " + member2.hashCode());
+        // 만약 equals와 hashCode를 오버라이딩하지 않았다면, 두 객체는 항상 다르다고 판단되고, 해시코드도 다르다.
+        // 오버라이딩했다면, id가 같으니 equals는 true, hashCode도 동일한 값이 출력된다.
     }
 }
--- src/collection/set/member/Member.java
+++ src/collection/set/member/Member.java
@@ -10,27 +10,35 @@ public class Member {
         this.id = id;
     }
 
-    public String getId() {
+    public String ring getId() {
         return id;
     }
 
     @Override
     public boolean equals(Object o) {
+        // 1. 자기 자신과 비교하는 경우 항상 true를 반환한다.
+        //    (동일한 객체이므로 무조건 같다.)
         if (this == o) return true;
+
+        // 2. 비교 대상이 null이거나, 클래스 타입이 다르면 false를 반환한다.
+        //    (null은 어떤 객체와도 같지 않고, 타입이 다르면 논리적으로 같다고 볼 수 없다.)
         if (o == null || getClass() != o.getClass()) return false;
+
+        // 3. 비교 대상 객체를 Member 타입으로 캐스팅한다.
         Member member = (Member) o;
+
+        // 실질적으로 오버라이딩 하는 이유는 이 부분이다.
+        // 4. 두 객체의 id 값이 같은지 비교한다.
+        // (Objects.equals는 null-safe 비교를 제공한다. 즉, 둘 다 null이면 true, 하나만 null이면 false, 둘 다 같으면 true)
         return Objects.equals(id, member.id);
     }
 
     @Override
     public int hashCode() {
+        // 5. id 필드를 기반으로 해시코드를 생성한다.
+        //    (Objects.hash는 null도 안전하게 처리해준다. id가 같으면 항상 같은 해시코드가 반환된다.)
+        //    -> equals가 true인 객체는 반드시 hashCode도 같아야 하므로, equals에서 비교하는 필드를 기준으로 hashCode를 만든다.
         return Objects.hash(id);
     }
 
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id='" + id + '\'' +
-                '}';
-    }
 }
